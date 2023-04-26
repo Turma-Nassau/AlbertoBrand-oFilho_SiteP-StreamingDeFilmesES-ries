54648a91f0b2f1c3c2d7963b248523695064267f
BettoBrandao <42591925+BettoBrandao@users.noreply.github.com>
2023-04-25 22:24:19 -0300

    Delete Movies-Project-main directory

--- Movies-Project-main/src/components/BasicComponents/BasicButton.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "../../styles/css/BasicButton.module.css";
-
-export default function BasicButton({ text }) {
-  return (
-    <>
-      <button className={styles.btn}>{text}</button>
-    </>
-  );
-}
